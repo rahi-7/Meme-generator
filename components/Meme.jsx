bb6ec0ec0f1658919deb3611b7b0001282592867
rahi-7 <rabera131@gmail.com>
2024-07-12 10:28:32 -0700

    first commit

---
+++ components/Meme.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+
+const Meme = () => {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemes, setAllMemesData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemesData(data));
+  }, []);
+
+  function getMemeImage() {
+    const memesArray = allMemes.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  function handleText(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+          onChange={handleText}
+          value={meme.topText}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+          onChange={handleText}
+          value={meme.bottomText}
+        />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
+};
+
+export default Meme;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import Meme from "../components/Meme";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Meme />
+    </div>
+  );
+};
+
+export default App;
